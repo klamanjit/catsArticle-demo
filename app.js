263ab3f9df45a02d98421981175fcfef0226623d
klamanjit <manjit.wasit@gmail.com>
2023-06-07 10:07:40 +0700

    add methods to Cat class

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var Cat = /** @class */ (function () {
     function Cat() {
+        this.numOfImg = 0;
         // selection properties
         this.btnOpenNav = document.querySelector(".btn-open");
         this.btnCloseNav = document.querySelector(".btn-close");
         this.containerDiv = document.querySelector(".container");
         this.navigatorSection = document.querySelector(".section-navigator");
-        // actve nav btns
+        this.searchBarContainer = document.querySelector(".search-container");
+        this.btnSearch = document.querySelector(".btn-search");
+        this.searchBarEl = document.querySelector(".search-bar");
+        this.imagsEl = document.querySelectorAll(".img");
+        this.progressBar = document.querySelector(".progress-bar");
+        this.progressCicles = document.querySelectorAll(".progress-circle");
+        this.btnNext = document.querySelector(".btn-next");
+        this.btnPrev = document.querySelector(".btn-prev");
+        // active nav btns
         this.openNav();
         this.closeNav();
+        // active search bar
+        this.toggleSearchBar();
+        // activate imgs expandion
+        this.expandingImgOnclick();
+        //Progression bar
+        this.adjustProgressionBar();
     }
     Cat.prototype.openNav = function () {
         var _this = this;
@@ -18,6 +33,11 @@ var Cat = /** @class */ (function () {
             // adjust btns position
             _this.btnCloseNav.style.transform = "rotate(0)";
             _this.btnOpenNav.style.transform = "rotate(-90deg)";
+            _this.navigatorSection.scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+                inline: "end",
+            });
         });
     };
     Cat.prototype.closeNav = function () {
@@ -30,6 +50,65 @@ var Cat = /** @class */ (function () {
             _this.btnCloseNav.style.transform = "rotate(90deg)";
             _this.btnOpenNav.style.transform = "rotate(0)";
         });
+    };
+    Cat.prototype.toggleSearchBar = function () {
+        var _this = this;
+        this.btnSearch.addEventListener("click", function (e) {
+            e.preventDefault();
+            _this.searchBarEl.classList.toggle("active");
+        });
+    };
+    Cat.prototype.removeActiveClassOnImgs = function () {
+        this.imagsEl.forEach(function (img) {
+            img.classList.remove("active");
+        });
+    };
+    Cat.prototype.expandingImgOnclick = function () {
+        var _this = this;
+        this.imagsEl.forEach(function (img) {
+            img.addEventListener("click", function () {
+                _this.removeActiveClassOnImgs();
+                img.classList.toggle("active");
+            });
+        });
+    };
+    Cat.prototype.expandingImgOnbtn = function (numImg) {
+        document.getElementById("img".concat(numImg)).style.flex = "5";
+    };
+    Cat.prototype.downSizeImg = function (numIng) {
+        document.getElementById("img".concat(numIng)).style.flex = "0.5";
+    };
+    Cat.prototype.adjustProgressionBar = function () {
+        var _this = this;
+        this.btnNext.addEventListener("click", function () {
+            _this.numOfImg++;
+            if (_this.numOfImg > _this.imagsEl.length) {
+                _this.numOfImg = _this.imagsEl.length;
+                return _this.numOfImg;
+            }
+            _this.numOfImg !== 1
+                ? _this.downSizeImg(_this.numOfImg - 1)
+                : _this.expandingImgOnbtn((_this.numOfImg = 1));
+            _this.expandingImgOnbtn(_this.numOfImg);
+            _this.progressBar.style.width = "".concat((_this.numOfImg - 1) * 25, "%");
+            var circle = document.getElementById("circle".concat(_this.numOfImg));
+            circle.style.borderColor = "#d97706";
+        });
+        this.btnPrev.addEventListener("click", function () {
+            _this.numOfImg--;
+            if (_this.numOfImg < 0) {
+                _this.numOfImg = 0;
+                return _this.numOfImg;
+            }
+            _this.numOfImg !== 5
+                ? _this.downSizeImg(_this.numOfImg + 1)
+                : _this.expandingImgOnbtn((_this.numOfImg = 5));
+            _this.expandingImgOnbtn(_this.numOfImg === 0 ? 1 : _this.numOfImg);
+            _this.progressBar.style.width = "".concat((_this.numOfImg - 1) * 25, "%");
+            var circle = document.getElementById("circle".concat(_this.numOfImg + 1));
+            circle.style.borderColor = "#e0e0e0";
+        });
+        return this.numOfImg;
     };
     return Cat;
 }());
--- app.ts
+++ app.ts
@@ -1,25 +1,64 @@
 class Cat {
-  private btnOpenNav: HTMLButtonElement;
-  private btnCloseNav: HTMLButtonElement;
-  private containerDiv: HTMLDivElement;
-  private navigatorSection: HTMLDivElement;
+  // nav
+  btnOpenNav: HTMLButtonElement;
+  btnCloseNav: HTMLButtonElement;
+  containerDiv: HTMLDivElement;
+  navigatorSection: HTMLDivElement;
+  // search bar
+  searchBarContainer: HTMLFormElement;
+  searchBarEl: HTMLInputElement;
+  btnSearch: HTMLButtonElement;
+  // images
+  imagsEl: NodeListOf<Element>;
+  // progression-bar
+  progressBar: HTMLDivElement;
+  progressCicles: NodeListOf<Element>;
+  btnNext: HTMLButtonElement;
+  btnPrev: HTMLButtonElement;
+
+  numOfImg: number = 0;
   constructor() {
     // selection properties
-    this.btnOpenNav = document.querySelector(".btn-open") as HTMLButtonElement;
+    this.btnOpenNav = document.querySelector(".btn-open")! as HTMLButtonElement;
     this.btnCloseNav = document.querySelector(
       ".btn-close"
-    ) as HTMLButtonElement;
-    this.containerDiv = document.querySelector(".container") as HTMLDivElement;
+    )! as HTMLButtonElement;
+    this.containerDiv = document.querySelector(".container")! as HTMLDivElement;
     this.navigatorSection = document.querySelector(
       ".section-navigator"
-    ) as HTMLDivElement;
+    )! as HTMLDivElement;
 
-    // actve nav btns
+    this.searchBarContainer = document.querySelector(
+      ".search-container"
+    )! as HTMLFormElement;
+    this.btnSearch = document.querySelector(
+      ".btn-search"
+    )! as HTMLButtonElement;
+    this.searchBarEl = document.querySelector(
+      ".search-bar"
+    )! as HTMLInputElement;
+    this.imagsEl = document.querySelectorAll(".img")!;
+    this.progressBar = document.querySelector(
+      ".progress-bar"
+    )! as HTMLDivElement;
+    this.progressCicles = document.querySelectorAll(".progress-circle")!;
+    this.btnNext = document.querySelector(".btn-next")! as HTMLButtonElement;
+    this.btnPrev = document.querySelector(".btn-prev")! as HTMLButtonElement;
+
+    // active nav btns
     this.openNav();
     this.closeNav();
+    // active search bar
+    this.toggleSearchBar();
+
+    // activate imgs expandion
+    this.expandingImgOnclick();
+
+    //Progression bar
+    this.adjustProgressionBar();
   }
 
-  openNav() {
+  private openNav() {
     this.btnOpenNav.addEventListener("click", () => {
       // add show-nav class
       this.containerDiv.classList.add("show-nav");
@@ -28,10 +67,16 @@ class Cat {
       // adjust btns position
       this.btnCloseNav.style.transform = `rotate(0)`;
       this.btnOpenNav.style.transform = `rotate(-90deg)`;
+
+      this.navigatorSection.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "end",
+      });
     });
   }
 
-  closeNav() {
+  private closeNav() {
     this.btnCloseNav.addEventListener("click", () => {
       // add show-nav class
       this.containerDiv.classList.remove("show-nav");
@@ -41,6 +86,80 @@ class Cat {
       this.btnCloseNav.style.transform = `rotate(90deg)`;
       this.btnOpenNav.style.transform = `rotate(0)`;
     });
+  }
+
+  private toggleSearchBar() {
+    this.btnSearch.addEventListener("click", (e: Event) => {
+      e.preventDefault();
+
+      this.searchBarEl.classList.toggle("active");
+    });
+  }
+
+  private removeActiveClassOnImgs() {
+    this.imagsEl.forEach((img) => {
+      (img as HTMLDivElement).classList.remove("active");
+    });
+  }
+
+  private expandingImgOnclick() {
+    this.imagsEl.forEach((img) => {
+      (img as HTMLDivElement).addEventListener("click", () => {
+        this.removeActiveClassOnImgs();
+        img.classList.toggle("active");
+      });
+    });
+  }
+
+  private expandingImgOnbtn(numImg: number) {
+    document.getElementById(`img${numImg}`)!.style.flex = `5`;
+  }
+
+  private downSizeImg(numIng: number) {
+    document.getElementById(`img${numIng}`)!.style.flex = `0.5`;
+  }
+
+  private adjustProgressionBar() {
+    this.btnNext.addEventListener("click", (): any => {
+      this.numOfImg++;
+
+      if (this.numOfImg > this.imagsEl.length) {
+        this.numOfImg = this.imagsEl.length;
+        return this.numOfImg;
+      }
+      this.numOfImg !== 1
+        ? this.downSizeImg(this.numOfImg - 1)
+        : this.expandingImgOnbtn((this.numOfImg = 1));
+
+      this.expandingImgOnbtn(this.numOfImg);
+      this.progressBar.style.width = `${(this.numOfImg - 1) * 25}%`;
+      const circle = document.getElementById(
+        `circle${this.numOfImg}`
+      ) as HTMLDivElement;
+      circle.style.borderColor = "#d97706";
+    });
+
+    this.btnPrev.addEventListener("click", (): any => {
+      this.numOfImg--;
+
+      if (this.numOfImg < 0) {
+        this.numOfImg = 0;
+        return this.numOfImg;
+      }
+
+      this.numOfImg !== 5
+        ? this.downSizeImg(this.numOfImg + 1)
+        : this.expandingImgOnbtn((this.numOfImg = 5));
+
+      this.expandingImgOnbtn(this.numOfImg === 0 ? 1 : this.numOfImg);
+      this.progressBar.style.width = `${(this.numOfImg - 1) * 25}%`;
+      const circle = document.getElementById(
+        `circle${this.numOfImg + 1}`
+      ) as HTMLDivElement;
+      circle.style.borderColor = "#e0e0e0";
+    });
+
+    return this.numOfImg;
   }
 }
 
