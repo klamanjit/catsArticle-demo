036213542ff7e46ac60723e43efbc40c95566e28
klamanjit <manjit.wasit@gmail.com>
2023-06-10 09:17:35 +0700

    add fn to check condition of inputs in register

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function Autobind(_, _2, descriptor) {
+    // const orinalMethod = descriptor.value;
+    console.log(descriptor);
+}
 var Cat = /** @class */ (function () {
     function Cat() {
         this.numOfImg = 0;
@@ -16,6 +20,9 @@ var Cat = /** @class */ (function () {
         this.btnPrev = document.querySelector(".btn-prev");
         this.registerContainer = document.querySelector(".register-container");
         this.footerSection = document.querySelector(".section-footer");
+        this.usernameEl = document.getElementById("user-name");
+        this.rateEl = document.getElementById("rate");
+        this.descriptionEl = document.getElementById("description");
         // active nav btns
         this.openNavOnBtn();
         this.closeNavOnBtn();
@@ -25,8 +32,11 @@ var Cat = /** @class */ (function () {
         this.closeRegisterOnBtn();
         this.activateContactLink();
         this.closeFooterOnBtn();
+        this.showContactInfo();
         // active search bar
         this.toggleSearchBar();
+        // activate submitBtn
+        this.activateSubmitBtn();
         // activate imgs expandion
         // this.expandingImgOnclick();
         //Progression bar
@@ -78,6 +88,8 @@ var Cat = /** @class */ (function () {
     Cat.prototype.activeHomeLink = function () {
         var _this = this;
         document.getElementById("home").addEventListener("click", function () {
+            _this.closeFooterSection();
+            _this.closeRegisterSection();
             _this.closeNav();
             _this.containerDiv.scrollIntoView({ behavior: "smooth" });
         });
@@ -85,6 +97,7 @@ var Cat = /** @class */ (function () {
     Cat.prototype.activateAboutLink = function () {
         var _this = this;
         document.getElementById("about").addEventListener("click", function () {
+            _this.closeFooterSection();
             _this.closeNav();
             _this.registerContainer.style.display = "flex";
             document.querySelector(".form-container").scrollIntoView({
@@ -94,14 +107,94 @@ var Cat = /** @class */ (function () {
             });
         });
     };
+    Cat.prototype.clearRegisInputs = function () {
+        this.usernameEl.value = "";
+        this.rateEl.value = "";
+        this.descriptionEl.value = "";
+    };
+    Cat.prototype.isValided = function (validateObj) {
+        var isValid = true;
+        if (validateObj.required) {
+            isValid = isValid && validateObj.value.toString().trim().length !== 0;
+        }
+        if (validateObj.minLength != null &&
+            typeof validateObj.value === "string") {
+            isValid = isValid && validateObj.value.length >= validateObj.minLength;
+        }
+        if (validateObj.min != null && typeof validateObj.value === "number") {
+            isValid = isValid && validateObj.value >= validateObj.min;
+        }
+        if (validateObj.max != null && typeof validateObj.value === "number") {
+            isValid = isValid && validateObj.value <= validateObj.max;
+        }
+        return isValid;
+    };
+    Cat.prototype.gatherRegisterInfo = function () {
+        var usernameInfo = this.usernameEl.value;
+        var rateInfo = this.rateEl.value;
+        var descriptionInfo = this.descriptionEl.value;
+        var validedUserNane = {
+            value: usernameInfo,
+            required: true,
+        };
+        var validRate = {
+            value: +rateInfo,
+            required: true,
+            min: 1,
+            max: 10,
+        };
+        var validDescription = {
+            value: descriptionInfo,
+            required: true,
+            minLength: 5,
+        };
+        if (this.isValided(validedUserNane) &&
+            this.isValided(validRate) &&
+            this.isValided(validDescription)) {
+            console.log([usernameInfo, rateInfo, descriptionInfo]);
+        }
+        else {
+            alert("The inputs is not valid, Please try again \uD83D\uDE05");
+            this.clearRegisInputs();
+        }
+    };
+    Cat.prototype.activateSubmitBtn = function () {
+        var _this = this;
+        document
+            .querySelector(".regis-submit-btn")
+            .addEventListener("click", function (e) {
+            e.preventDefault();
+            _this.gatherRegisterInfo();
+            _this.clearRegisInputs();
+        });
+    };
     Cat.prototype.activateContactLink = function () {
         var _this = this;
         document.getElementById("contact").addEventListener("click", function () {
+            _this.closeRegisterSection();
             _this.closeNav();
             _this.footerSection.style.display = "block";
             document
                 .querySelector(".footer-container")
                 .scrollIntoView({ behavior: "smooth" });
+        });
+    };
+    Cat.prototype.showContactInfo = function () {
+        document
+            .querySelector(".footer-container")
+            .addEventListener("mouseover", function (e) {
+            // console.log(e.target);
+            var iconTarget = e.target.classList.contains("contacts-icon");
+            if (iconTarget) {
+                document.querySelectorAll(".contact-text").forEach(function (text) {
+                    text.style.display = "block";
+                });
+            }
+            else {
+                document.querySelectorAll(".contact-text").forEach(function (text) {
+                    text.style.display = "none";
+                });
+            }
         });
     };
     Cat.prototype.toggleSearchBar = function () {
@@ -180,4 +273,3 @@ var Cat = /** @class */ (function () {
     return Cat;
 }());
 var cat1 = new Cat();
-console.log(cat1.numOfImg);
--- app.ts
+++ app.ts
@@ -1,3 +1,16 @@
+interface Validable {
+  value: string | number;
+  required: boolean;
+  minLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  // const orinalMethod = descriptor.value;
+  console.log(descriptor);
+}
+
 class Cat {
   // nav
   btnOpenNav: HTMLButtonElement;
@@ -17,6 +30,9 @@ class Cat {
   btnPrev: HTMLButtonElement;
   // section-register
   registerContainer: HTMLDivElement;
+  usernameEl: HTMLInputElement;
+  rateEl: HTMLInputElement;
+  descriptionEl: HTMLTextAreaElement;
   // section-footer
   footerSection: HTMLDivElement;
 
@@ -55,6 +71,11 @@ class Cat {
     this.footerSection = document.querySelector(
       ".section-footer"
     )! as HTMLDivElement;
+    this.usernameEl = document.getElementById("user-name")! as HTMLInputElement;
+    this.rateEl = document.getElementById("rate")! as HTMLInputElement;
+    this.descriptionEl = document.getElementById(
+      "description"
+    )! as HTMLTextAreaElement;
 
     // active nav btns
     this.openNavOnBtn();
@@ -66,8 +87,11 @@ class Cat {
     this.closeRegisterOnBtn();
     this.activateContactLink();
     this.closeFooterOnBtn();
+    this.showContactInfo();
     // active search bar
     this.toggleSearchBar();
+    // activate submitBtn
+    this.activateSubmitBtn();
 
     // activate imgs expandion
     // this.expandingImgOnclick();
@@ -132,6 +156,8 @@ class Cat {
 
   private activeHomeLink() {
     document.getElementById("home")!.addEventListener("click", () => {
+      this.closeFooterSection();
+      this.closeRegisterSection();
       this.closeNav();
       this.containerDiv.scrollIntoView({ behavior: "smooth" });
     });
@@ -139,6 +165,7 @@ class Cat {
 
   private activateAboutLink() {
     document.getElementById("about")!.addEventListener("click", () => {
+      this.closeFooterSection();
       this.closeNav();
 
       this.registerContainer.style.display = `flex`;
@@ -150,14 +177,113 @@ class Cat {
     });
   }
 
+  private clearRegisInputs() {
+    this.usernameEl.value = "";
+    this.rateEl.value = "";
+    this.descriptionEl.value = "";
+  }
+
+  private isValided(validateObj: Validable) {
+    let isValid = true;
+
+    if (validateObj.required) {
+      isValid = isValid && validateObj.value.toString().trim().length !== 0;
+    }
+
+    if (
+      validateObj.minLength != null &&
+      typeof validateObj.value === "string"
+    ) {
+      isValid = isValid && validateObj.value.length >= validateObj.minLength;
+    }
+
+    if (validateObj.min != null && typeof validateObj.value === "number") {
+      isValid = isValid && validateObj.value >= validateObj.min;
+    }
+
+    if (validateObj.max != null && typeof validateObj.value === "number") {
+      isValid = isValid && validateObj.value <= validateObj.max;
+    }
+
+    return isValid;
+  }
+
+  private gatherRegisterInfo() {
+    const usernameInfo = this.usernameEl.value;
+    const rateInfo = this.rateEl.value;
+    const descriptionInfo = this.descriptionEl.value;
+
+    const validedUserNane: Validable = {
+      value: usernameInfo,
+      required: true,
+    };
+
+    const validRate: Validable = {
+      value: +rateInfo,
+      required: true,
+      min: 1,
+      max: 10,
+    };
+
+    const validDescription: Validable = {
+      value: descriptionInfo,
+      required: true,
+      minLength: 5,
+    };
+
+    if (
+      this.isValided(validedUserNane) &&
+      this.isValided(validRate) &&
+      this.isValided(validDescription)
+    ) {
+      console.log([usernameInfo, rateInfo, descriptionInfo]);
+    } else {
+      alert(`The inputs is not valid, Please try again 😅`);
+      this.clearRegisInputs();
+    }
+  }
+
+  private activateSubmitBtn() {
+    document
+      .querySelector(".regis-submit-btn")!
+      .addEventListener("click", (e: Event) => {
+        e.preventDefault();
+        this.gatherRegisterInfo();
+        this.clearRegisInputs();
+      });
+  }
+
   private activateContactLink() {
     document.getElementById("contact")!.addEventListener("click", () => {
+      this.closeRegisterSection();
       this.closeNav();
       this.footerSection.style.display = `block`;
       document
         .querySelector(".footer-container")!
         .scrollIntoView({ behavior: "smooth" });
     });
+  }
+
+  private showContactInfo() {
+    document
+      .querySelector(".footer-container")!
+      .addEventListener("mouseover", (e: Event) => {
+        // console.log(e.target);
+
+        const iconTarget = (e.target! as HTMLElement).classList.contains(
+          "contacts-icon"
+        );
+
+        if (iconTarget) {
+          document.querySelectorAll(".contact-text")!.forEach((text) => {
+            (text as HTMLSpanElement).style.display = `block`;
+          });
+        } else {
+          document.querySelectorAll(".contact-text")!.forEach((text) => {
+            (text as HTMLSpanElement).style.display = `none`;
+          });
+        }
+      });
   }
 
   private toggleSearchBar() {
@@ -260,4 +386,3 @@ class Cat {
 }
 
 const cat1 = new Cat();
-console.log(cat1.numOfImg);
